1f6856586c99f432b3440aaf5ac8e571c147da22
casoon <joern.seidel@casoon.de>
2021-11-30 11:26:06 +0100

    Update ShipmentEndpoint.cs

--- Billbee.Net/Endpoints/ShipmentEndpoint.cs
+++ Billbee.Net/Endpoints/ShipmentEndpoint.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Billbee.Net.Exceptions;
 using Billbee.Net.Models;
+using Billbee.Net.Models.Rechnungsdruck.WebApp.Model.Api;
 
 namespace Billbee.Net.Endpoints
 {
@@ -18,8 +19,81 @@ namespace Billbee.Net.Endpoints
     {
         public ShipmentEndpoint(IBillbeeClient billbeeClient) : base(billbeeClient)
         {
-            this.EndPoint = "";
+            this.EndPoint = "shipment";
         }
+
+        public async Task<ShippingProvider> GetShippingProviderAsync()
+        {
+            try
+            {
+                var result = await billbeeClient.GetAsync<ShippingProvider>(this.EndPoint + "/shippingproviders");
+                return result;
+            }
+            catch (NotFoundException)
+            {
+                throw;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<ShippingCarrier> GetShippingCarriersAsync()
+        {
+            try
+            {
+                var result = await billbeeClient.GetAsync<ShippingCarrier>(this.EndPoint + "/shippingcarriers");
+                return result;
+            }
+            catch (NotFoundException)
+            {
+                throw;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<dynamic> AddShipmentAsync(PostShipment shipment)
+        {
+            try
+            {
+                var result = await billbeeClient.AddAsync<dynamic>(this.EndPoint + "/shipment", shipment);
+                return result;
+            }
+            catch (NotFoundException)
+            {
+                throw;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<dynamic> ShipOrderWithLabel(ShipmentWithLabel shipment)
+        {
+            try
+            {
+                var result = await billbeeClient.AddAsync<dynamic>(this.EndPoint + "/shipment", shipment);
+                return result;
+            }
+            catch (NotFoundException)
+            {
+                throw;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+
+
     }
+
+
 }
 
