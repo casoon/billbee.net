c157792af2a5ed1c7f357df481bfb3839d6ee88b
casoon <joern.seidel@casoon.de>
2021-11-30 13:52:19 +0100

    Update CustomerAddressEndpoint.cs

--- Billbee.Net/Endpoints/CustomerAddressEndpoint.cs
+++ Billbee.Net/Endpoints/CustomerAddressEndpoint.cs
@@ -9,16 +9,94 @@ namespace Billbee.Net.Endpoints
 {
     public interface ICustomerAddressEndpoint : IBaseEndpoint
     {
-
+        Task<List<CustomerAddress>> GetAllAsync(int page = 0, int pageSize = 50);
+        Task<CustomerAddress> AddAsync(CustomerAddress address);
+        Task<CustomerAddress> GetAsync(long id);
+        Task<CustomerAddress> UpdateAsync(CustomerAddress address);
     }
 
-    public class CustomerAddressEndpoint : ExtendedEndpoint<Address>, ICustomerAddressEndpoint
+    public class CustomerAddressEndpoint : BaseEndpoint, ICustomerAddressEndpoint
     {
         public CustomerAddressEndpoint(IBillbeeClient billbeeClient) : base(billbeeClient)
         {
             this.EndPoint = "customer-addresses";
         }
 
+
+        public async Task<CustomerAddress> AddAsync(CustomerAddress address)
+        {
+            var queryParams = new Dictionary<string, string>();
+
+            try
+            {
+                var result = await billbeeClient.AddAsync<CustomerAddress>(this.EndPoint, address, queryParams);
+                return result;
+            }
+            catch (NotFoundException)
+            {
+                throw;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<CustomerAddress> GetAsync(long id)
+        {
+            try
+            {
+                var result = await billbeeClient.GetAsync<CustomerAddress>(this.EndPoint + "/" + id.ToString());
+                return result;
+            }
+            catch (NotFoundException)
+            {
+                throw;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<CustomerAddress> UpdateAsync(CustomerAddress address)
+        {
+            try
+            {
+                var result = await billbeeClient.UpdateAsync<CustomerAddress>(this.EndPoint + "/" + address.Id.ToString(), address);
+                return result;
+            }
+            catch (NotFoundException)
+            {
+                throw;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+
+        public async Task<List<CustomerAddress>> GetAllAsync(int page = 0, int pageSize = 50)
+        {
+            var queryParams = new Dictionary<string, string>();
+            queryParams.Add("page", page.ToString());
+            queryParams.Add("pageSize", pageSize.ToString());
+
+            try
+            {
+                var result = await billbeeClient.GetAllAsync<CustomerAddress>(this.EndPoint, queryParams);
+                return result;
+            }
+            catch (NotFoundException)
+            {
+                throw;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
 
     }
 }
