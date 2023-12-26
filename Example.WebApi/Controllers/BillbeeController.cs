dd0ff34d84f91ea360eb04d99ca97633003946d0
casoon <joern.seidel@casoon.de>
2023-12-26 13:01:31 +0100

    added example WebApi

--- Example.WebApi/Controllers/BillbeeController.cs
+++ Example.WebApi/Controllers/BillbeeController.cs
@@ -1,10 +1,8 @@
 using Billbee.Net.Endpoints;
-using Billbee.Net.Enums;
 using Billbee.Net.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Example.WebApi.Controllers;
-
 
 [ApiController]
 [Produces("application/json")]
@@ -12,7 +10,7 @@ namespace Example.WebApi.Controllers;
 public class BillbeeController : ControllerBase
 {
     private readonly IOrderEndpoint _orders;
-    
+
     public BillbeeController(IOrderEndpoint orders)
     {
         _orders = orders;
@@ -20,25 +18,25 @@ public class BillbeeController : ControllerBase
 
 
     /// <summary>
-    /// Action to get a list of all orders in the database.
+    ///     Action to get a list of all orders in the database.
     /// </summary>
     /// <returns>Returns a list of all orders since yesterday in the database</returns>
-    /// /// <response code="200">Returns a list of all orders in the database</response>
-    /// /// <response code="400">Returned if the orders couldn't be loaded</response>
+    /// ///
+    /// <response code="200">Returns a list of all orders in the database</response>
+    /// ///
+    /// <response code="400">Returned if the orders couldn't be loaded</response>
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    [HttpGet("orders")]
-    public ActionResult<List<Order>> Orders()
+    [HttpGet("example")]
+    public ActionResult<List<Order>> Example()
     {
         try
         {
-            return new List<Order>();
-            //return _orders.GetAllAsync(minOrderDate: DateTime.Today.AddDays(-1)).GetAwaiter().GetResult();
+            return _orders.GetAllAsync(minOrderDate: DateTime.Today.AddDays(-3)).GetAwaiter().GetResult();
         }
         catch (Exception ex)
         {
             return BadRequest(ex.Message);
         }
     }
-
 }
