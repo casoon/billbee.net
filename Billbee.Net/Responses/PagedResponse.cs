a7fe68a27a9110333fb186dd3a812693ed05111b
casoon <joern.seidel@casoon.de>
2024-07-03 22:45:30 +0200

    version increment

--- Billbee.Net/Responses/PagedResponse.cs
+++ Billbee.Net/Responses/PagedResponse.cs
@@ -1,28 +1,28 @@
-﻿using System.Collections.Generic;
+﻿#nullable enable
+using System.Collections.Generic;
 
-namespace Billbee.Net.Responses
+namespace Billbee.Net.Responses;
+
+public class PagedResponse<T>
 {
-    public class PagedResponse<T>
-    {
-        public int ErrorCode { get; set; }
+    public int ErrorCode { get; set; }
 
-        public string? ErrorDescription { get; set; }
+    public string? ErrorDescription { get; set; }
 
-        public string? ErrorMessage { get; set; }
+    public string? ErrorMessage { get; set; }
 
-        public List<T> Data { get; set; }
+    public List<T> Data { get; set; }
 
-        public PagingInformation Paging { get; set; }
-    }
+    public PagingInformation Paging { get; set; }
+}
 
-    public class PagingInformation
-    {
-        public int Page { get; set; }
+public class PagingInformation
+{
+    public int Page { get; set; }
 
-        public int TotalPages { get; set; }
+    public int TotalPages { get; set; }
 
-        public int TotalRows { get; set; }
+    public int TotalRows { get; set; }
 
-        public int PageSize { get; set; }
-    }
+    public int PageSize { get; set; }
 }
