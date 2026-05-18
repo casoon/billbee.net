9c88138473de780b67058e9c2c5645e1ec201076
Jörn Seidel <joern.seidel@casoon.de>
2026-05-18 20:15:15 +0200

    refactor: modernize example Program.cs files

--- Example.ConsoleApp/Program.cs
+++ Example.ConsoleApp/Program.cs
@@ -2,20 +2,18 @@ using Billbee.Net;
 using Billbee.Net.Endpoints;
 using Billbee.Net.Enums;
 using Billbee.Net.Exceptions;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-IConfiguration configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.RegisterBillbee(builder.Configuration);
 
-using var host = CreateHostBuilder(args).Build();
+using var host = builder.Build();
 using var scope = host.Services.CreateScope();
-
-var services = scope.ServiceProvider;
 
 try
 {
-    var orders = services.GetRequiredService<IOrderEndpoint>();
+    var orders = scope.ServiceProvider.GetRequiredService<IOrderEndpoint>();
     var orderStates = new List<OrderStateEnum> { OrderStateEnum.Im_Fulfillment };
     var result = await orders.GetAllAsync(orderStateId: orderStates);
     foreach (var order in result)
@@ -32,10 +30,4 @@ catch (ApiException ex)
 catch (Exception ex)
 {
     Console.WriteLine($"Unexpected error: {ex.Message}");
-}
-
-IHostBuilder CreateHostBuilder(string[] strings)
-{
-    return Host.CreateDefaultBuilder()
-        .ConfigureServices((_, services) => { services.RegisterBillbee(configuration); });
 }
--- Example.WorkerService/Program.cs
+++ Example.WorkerService/Program.cs
@@ -1,48 +1,34 @@
 using Billbee.Net;
+using Example.WorkerService;
 using Quartz;
 using Quartz.AspNetCore;
 
+var builder = Host.CreateApplicationBuilder(args);
 
-namespace Example.WorkerService;
+builder.Services.RegisterBillbee(builder.Configuration);
 
-public class Program
+builder.Services.AddQuartz(quartz =>
 {
-    public static void Main(string[] args)
-    {
-        IConfiguration configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
-        
-        var builder = Host.CreateApplicationBuilder(args);
-        
-        builder.Services.RegisterBillbee(configuration);
-        
-        builder.Services.AddQuartz(quartz =>
-        {
-            quartz.SchedulerId = "Scheduler-Core";
-            quartz.SchedulerName = "Billbee";
-            quartz.UseSimpleTypeLoader();
-            quartz.UseInMemoryStore();
-            quartz.UseDefaultThreadPool(tp => { tp.MaxConcurrency = 3; });
-            
-            quartz.AddJob<ProcessNewOrders>(j => j.WithIdentity("ProcessNewOrders"));
-                
-            quartz.AddTrigger(t => t
-                .ForJob("ProcessNewOrders")
-                .StartNow()
-                .WithSimpleSchedule
-                (s =>
-                    s.WithInterval(TimeSpan.FromMinutes(5))
-                        .RepeatForever()
-                ));
-        
-        });
-        
-        builder.Services.AddQuartzServer(options =>
-        {
-            // when shutting down we want jobs to complete gracefully
-            options.WaitForJobsToComplete = true;
-        });
-        
-        var host = builder.Build();
-        host.Run();
-    }
-}
+    quartz.SchedulerId = "Scheduler-Core";
+    quartz.SchedulerName = "Billbee";
+    quartz.UseSimpleTypeLoader();
+    quartz.UseInMemoryStore();
+    quartz.UseDefaultThreadPool(tp => { tp.MaxConcurrency = 3; });
+
+    quartz.AddJob<ProcessNewOrders>(j => j.WithIdentity("ProcessNewOrders"));
+
+    quartz.AddTrigger(t => t
+        .ForJob("ProcessNewOrders")
+        .StartNow()
+        .WithSimpleSchedule(s =>
+            s.WithInterval(TimeSpan.FromMinutes(5))
+             .RepeatForever()
+        ));
+});
+
+builder.Services.AddQuartzServer(options =>
+{
+    options.WaitForJobsToComplete = true;
+});
+
+await builder.Build().RunAsync();
